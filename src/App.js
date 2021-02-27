0bc33ccf2a486b205e35195496b821aeeb8ffd83
Rub4l1to <adrian.perez.garcia2018@gmail.com>
2021-02-27 05:04:49 +0100

    Query and print with axios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
-import React from 'react'
+import axios from 'axios';
+import React, { Fragment, useEffect, useState } from 'react'
+
+// Components
+import { Card, Footer, Header } from './components/index';
 
 const App = () => {
+  const [list, setList] = useState([]);
+
+  useEffect(() =>
+    getCharacters(), [])
+
+  const getCharacters = async () => {
+    const data = await axios.get("https://rickandmortyapi.com/api/character/?page=1");
+    const res = await data.data.results;
+    setList(res)
+  }
+
+
+  const handleFilter = (search) =>
+    setList(list.filter((item) =>
+      search ?
+        item.name.toLowerCase().includes(search.toLowerCase())
+        :
+        getCharacters()
+    ))
+
   return (
-    <div>
-      <h1>Hola</h1>
-    </div>
+    <Fragment>
+      <Header {...{ handleFilter }} />
+      <main className="container">
+        <section className="grid-cards">
+          {list.slice(0, 20).map(item => (
+            <Card key={item.id} {...{ item }} />
+          ))}
+        </section>
+      </main>
+      <Footer />
+    </Fragment>
   )
 }
 
